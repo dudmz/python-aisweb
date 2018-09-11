f745da6200e4f2ccb609fb42d4cc7ac86d461f0c
Carlos Damázio <kaduardobsb95@gmail.com>
2018-09-11 11:08:14 -0300

    Added JSON response conversion

--- python_aisweb/python_aisweb.py
+++ python_aisweb/python_aisweb.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
+import json
 
 import requests
+import xmltodict
 
 
 class AISWEB(object):
 
     def __init__(self, API, API_PASS):
-        self.API = API
-        self.API_PASS = API_PASS
-        self.API_BASE = 'https://www.aisweb.aer.mil.br/api/'
+        self.__API = API
+        self.__API_PASS = API_PASS
+        self.__API_BASE = 'https://www.aisweb.aer.mil.br/api/'
 
     def __getattr__(self, name):
-
-        def wrapper(data={}, method='GET'):
-            response = self.request(name, data, method)
+        def wrapper(data={}, method='GET', response_type='XML'):
+            response = self.request(name, data, method, response_type)
             return response
-
         return wrapper
 
-    def request(self, name, data, method):
-
+    def request(self, name, data, method, response_type):
         built_req = '{}?apiKey={}&apiPass={}&area={}'.format(
             self.API_BASE, self.API, self.API_PASS, name
         )
@@ -37,12 +36,17 @@ class AISWEB(object):
                         "Endpoint does not exist or incorrect data entry,"
                         " check the docs!"
                     )
-                else:
-                    return response
+
+                if response_type == 'XML':
+                    return response.content
+                elif response_type == 'JSON':
+                    return self._convert_response_json(response.content)
 
         except Exception:
             raise Exception("Error: method {} not supported at this time!".format(
                 method
             ))
 
-        return
+    @staticmethod
+    def _convert_response_json(response_content):
+        return json.dumps(xmltodict.parse(response_content), indent=4)
